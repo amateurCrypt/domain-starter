5ae9b5c7ab4cb6cffeb6cb3eec1531985d220c9a
moh <muhammad.qasim1@live.com>
2022-11-14 02:15:06 -0500

    frontend set up. ability to connect to meta mask added.

--- src/App.js
+++ src/App.js
@@ -1,38 +1,94 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 import './styles/App.css';
 import twitterLogo from './assets/twitter-logo.svg';
 
 // Constants
-const TWITTER_HANDLE = '_buildspace';
+const TWITTER_HANDLE = 'testX';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const App = () => {
+	const [currentAccount, setCurrentAccount] = useState('');
+
+	const connectWallet = async () => {
+		try {
+			const { ethereum } = window;
+	  
+			if (!ethereum) {
+			  alert("Get MetaMask -> https://metamask.io/");
+			  return;
+			}
+
+			const accounts = await ethereum.request({ method: "eth_requestAccounts" });
+
+			console.log("Connected", accounts[0]);
+			setCurrentAccount(accounts[0]);
+		  } catch (error) {
+			console.log(error)
+		  }
+		}
+
+		const checkIfWalletIsConnected = async () => {
+			const { ethereum } = window;
+
+			if (!ethereum) {
+				console.log('Make sure you have metamask!');
+				return;
+			} else {
+				console.log('We have the ethereum object', ethereum);
+			  }
+
+			  const accounts = await ethereum.request({ method: 'eth_accounts' });
+
+
+	if (accounts.length !== 0) {
+      const account = accounts[0];
+      console.log('Found an authorized account:', account);
+      setCurrentAccount(account);
+    } else {
+      console.log('No authorized account found');
+    }
+  };
+
+  const renderNotConnectedContainer = () => (
+    <div className="connect-wallet-container">
+      <img src="https://media.giphy.com/media/L59aKIC2MFyfUfrz3n/giphy.gif" alt="ETH-logo" />
+      <button onClick={connectWallet} className="cta-button connect-wallet-button">
+        Connect Wallet
+      </button>
+    </div>
+  );
+
+  useEffect(() => {
+    checkIfWalletIsConnected();
+  }, []);
 
   return (
-		<div className="App">
-			<div className="container">
-
-				<div className="header-container">
-					<header>
+    <div className="App">
+      <div className="container">
+        <div className="header-container">
+          <header>
             <div className="left">
-              <p className="title">🐱‍👤 Ninja Name Service</p>
-              <p className="subtitle">Your immortal API on the blockchain!</p>
+            <p className="title">🐱‍👤 testX Name Service</p>
+              <p className="subtitle">This is where you mint testX domain names on Polygon testnet</p>
             </div>
-					</header>
-				</div>
+          </header>
+        </div>
+
+		{/* Hide the connect button if currentAccount isn't empty*/}
+        {!currentAccount && renderNotConnectedContainer()}
 
         <div className="footer-container">
-					<img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
-					<a
-						className="footer-text"
-						href={TWITTER_LINK}
-						target="_blank"
-						rel="noreferrer"
-					>{`built with @${TWITTER_HANDLE}`}</a>
-				</div>
-			</div>
-		</div>
-	);
-}
+          <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
+          <a className="footer-text" 
+            href={TWITTER_LINK} 
+            target="_blank"
+            rel="noreferrer">
+              {`built with @${TWITTER_HANDLE}`}
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default App;
